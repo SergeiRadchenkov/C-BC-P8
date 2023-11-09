7e027fb582937338d2541b93c17dfffa5c75f7b7
Sergei <radchenkovsa@yandex.ru>
2023-11-09 22:45:28 +0300

    Быстрая сортировка

---
+++ case1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Рекурсия
+// Напишите программу, которая сложит два числа a и b, без прямого сложения
+int SumNumbers(int x, int y) // x - 1, y + 1
+{
+    if (x == 0)
+        return y;
+    return SumNumbers(x - 1, y + 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите два числа: ");
+int[] numbers = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray(); // Числа нужно вводить через пробел
+Console.WriteLine($"Результат {numbers[0]} + {numbers[1]} = {SumNumbers(numbers[0], numbers[1])}");
---
+++ case2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Быстрая сортировка O(n * log2(n))
+/*
+[7, 4, 1, 3, 5, 2, 6]
+pivot = 7
+[4, 1, 3, 5, 2, 6] + [7] + []
+pivot = 4
+[1, 3, 2] + [4] + [5, 6]
+pivot = 1
+[] + [1] + [3, 2]
+pivot = 5
+[] + [5] + [6]
+pivot = 3
+[2] + [3] + []
+[1, 2, 3, 4, 5, 6, 7]
+*/
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21);
+}
+
+int[] QuickSort(int[] array, int leftIndex, int rightIndex)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}] ({leftIndex}, {rightIndex})"); // Чтобы видеть этапы сортировки
+    int i = leftIndex, j = rightIndex, pivot = array[leftIndex];
+    while (i <= j)
+    {
+        while (array[i] < pivot)
+        {
+            i++;
+        }
+        while (array[j] > pivot)
+        {
+            j--;
+        }
+        if (i <= j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    if (leftIndex < j)
+        QuickSort(array, leftIndex, j);
+    if (i < rightIndex)
+        QuickSort(array, i, rightIndex);
+
+    return array;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечный массив: [{string.Join(", ", QuickSort(array, 0, array.Length - 1))}]");
